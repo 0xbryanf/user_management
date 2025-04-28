95dc3f3b5aeda7416c9560b664bc582b969b3ef5
Bryan Factor <0xbryanf@gmail.com>
2025-04-29 02:51:53 +0800

    Modified the password strength bar logic

--- client/components/organisms/signUpForm.tsx
+++ client/components/organisms/signUpForm.tsx
@@ -44,11 +44,15 @@ const SignUpForm = ({
         password={password}
         validation={passwordValidation}
       />
+      <PasswordStrengthBar
+        password={password}
+        passwordStrength={passwordStrength}
+      />
       <ConfirmPasswordField
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
       />
-      <PasswordStrengthBar passwordStrength={passwordStrength} />
+
       <Button
         disabled={loading}
         className={`flex w-full justify-center rounded-md bg-gradient-to-r from-blue-600 via-indigo-500 to-sky-400 bg-opacity-60 backdrop-blur-md px-3 py-2 text-base font-semibold text-white shadow-md transition duration-200 ease-in-out focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-sky-500 ${
